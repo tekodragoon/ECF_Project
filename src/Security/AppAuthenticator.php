d634461ad1ac817e43ab8dcabb8e6224c86fefb9
VERBRUGGEN Frédéric <tekodragoon@gmail.com>
2023-01-22 18:51:31 +0100

    Remove useless comment

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -46,7 +46,6 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        // For example:
         return new RedirectResponse($this->urlGenerator->generate('app_home'));
     }
 
