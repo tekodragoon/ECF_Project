622c5d277a9bd7b27cb93a38e7e30404f7effcf1
VERBRUGGEN Frédéric <tekodragoon@gmail.com>
2023-01-23 23:52:53 +0100

    Change constraints message in French. Remove useless import

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,12 +4,10 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
@@ -27,11 +25,11 @@ class RegistrationFormType extends AbstractType
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Entrez un mot de passe',
                     ]),
                     new Length([
                         'min' => 8,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => '{{ limit }} caractères au minimum.',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
