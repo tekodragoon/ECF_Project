e7cd50889ee688526404fee63ec37b8307372f5d
VERBRUGGEN Frédéric <tekodragoon@gmail.com>
2023-01-24 00:05:27 +0100

    Add constraints

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -8,6 +8,8 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class UserType extends AbstractType
 {
@@ -15,13 +17,28 @@ class UserType extends AbstractType
     {
         $builder
             ->add('lastname', TextType::class, [
-                'label' => 'Nom'
+                'label' => 'Nom',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez renseigner votre nom.'
+                    ])
+                ]
             ])
             ->add('firstname', TextType::class, [
-                'label' => 'Prénom'
+                'label' => 'Prénom',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez renseigner votre prénom.'
+                    ])
+                ]
             ])
             ->add('email', EmailType::class, [
-                'label' => 'Email'
+                'label' => 'Email',
+                'constraints' => [
+                    new Email([
+                        'message' => 'Veuillez renseigner une adresse valide.'
+                    ])
+                ]
             ])
         ;
     }
