8911e4349c2ddee17afc2aed2620ee6d9cab4bcc
VERBRUGGEN Frédéric <tekodragoon@gmail.com>
2023-01-23 21:44:53 +0100

    Add account page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,4 +13,22 @@ class HomeController extends AbstractController
     {
         return $this->render('home/index.html.twig');
     }
+
+    #[Route('/account', name: 'app_account')]
+    public function account(): Response
+    {
+        return $this->render('account/account.html.twig');
+    }
+
+    #[Route('/account/show', name: 'app_account_show')]
+    public function showAccount(): Response
+    {
+        return $this->render('account/show_account.html.twig');
+    }
+
+    #[Route('/account/edit', name: 'app_account_edit')]
+    public function editAccount(): Response
+    {
+        return $this->render('account/edit_account.html.twig');
+    }
 }
