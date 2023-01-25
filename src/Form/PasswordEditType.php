5cf04b63f49ea8ebd91e12dd98060769c7d54b6b
VERBRUGGEN Frédéric <tekodragoon@gmail.com>
2023-01-25 21:23:43 +0100

    Bug Fix. Rename class

--- src/Form/PasswordEditType.php
+++ src/Form/PasswordEditType.php
@@ -3,12 +3,13 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
 use Symfony\Component\Validator\Constraints\Length;
 
-class PasswordType extends AbstractType
+class PasswordEditType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
@@ -32,13 +33,12 @@ class PasswordType extends AbstractType
                             // max length allowed by Symfony for security reasons
                             'max' => 4096,
                         ]),
-                    ]
+                    ],
                 ],
                 'second_options' => [
                     'label' => 'Verification',
-
                 ],
-                'invalid_message' => 'Verification incorrecte.'
+                'invalid_message' => 'Verification incorrecte.',
             ]);
     }
 }
