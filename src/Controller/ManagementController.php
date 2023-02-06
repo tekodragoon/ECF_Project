a46401915aed143abc4f6bcdf700a6c13686a404
VERBRUGGEN Frédéric <tekodragoon@gmail.com>
2023-02-06 23:36:07 +0100

    Create Management Controller.

---
+++ src/Controller/ManagementController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[IsGranted('ROLE_ADMIN')]
+#[Route('/manager')]
+class ManagementController extends AbstractController
+{
+    #[Route('/', name: 'app_management')]
+    public function index(): Response
+    {
+        return $this->render('management/index.html.twig', [
+            'controller_name' => 'ManagementController',
+        ]);
+    }
+}
