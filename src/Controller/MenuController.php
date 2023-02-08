489ffa45e69f526590abdc46ff835cde05a29726
VERBRUGGEN Frédéric <tekodragoon@gmail.com>
2023-02-08 22:55:17 +0100

    Show restaurant menu

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\RecipeType;
 use App\Repository\MenuRepository;
+use App\Repository\RecipeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,12 +12,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class MenuController extends AbstractController
 {
     #[Route('/menu', name: 'app_menu')]
-    public function index(MenuRepository $menuRepository): Response
+    public function index(MenuRepository $menuRepository, RecipeRepository $recipeRepository): Response
     {
         $menus = $menuRepository->findAll();
+        $entrees = $recipeRepository->findByType(RecipeType::entree);
+        $main = $recipeRepository->findByType(RecipeType::main);
+        $desserts = $recipeRepository->findByType(RecipeType::dessert);
 
         return $this->render('menu/index.html.twig', [
             'menus' => $menus,
+            'entrees' => $entrees,
+            'main' => $main,
+            'desserts' => $desserts,
         ]);
     }
 }
