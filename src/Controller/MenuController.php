898e51f25cc2d15897d12e3dea51f2c1570afe1d
VERBRUGGEN Frédéric <tekodragoon@gmail.com>
2023-02-08 21:45:04 +0100

    Show menu information

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\MenuRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class MenuController extends AbstractController
 {
     #[Route('/menu', name: 'app_menu')]
-    public function index(): Response
+    public function index(MenuRepository $menuRepository): Response
     {
-        return $this->render('menu/index.html.twig');
+        $menus = $menuRepository->findAll();
+
+        return $this->render('menu/index.html.twig', [
+            'menus' => $menus,
+        ]);
     }
 }
