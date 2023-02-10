0391d3c164d93b1c3862d9066fe799b1c44cda77
VERBRUGGEN Frédéric <tekodragoon@gmail.com>
2023-02-10 15:36:12 +0100

    Design list of all menus

--- src/Controller/ManagementController.php
+++ src/Controller/ManagementController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Form\MenuType;
+use App\Repository\MenuRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -15,5 +18,21 @@ class ManagementController extends AbstractController
     public function index(): Response
     {
         return $this->render('management/index.html.twig');
+    }
+
+    #[Route('/show', name: 'app_management_show')]
+    public function showManagementMenu(): Response
+    {
+        return $this->render('management/section.html.twig');
+    }
+
+    #[Route('/manage-menu', name:'app_management_menu')]
+    public function menu(Request $request, MenuRepository $menuRepository): Response
+    {
+        $menus = $menuRepository->findAll();
+
+        return $this->render('management/menu/menu-gestion.html.twig', [
+            'menus' => $menus,
+        ]);
     }
 }
