9ede92ded0210009647f74e13907a7ccb3bcd9e6
VERBRUGGEN Frédéric <tekodragoon@gmail.com>
2023-03-18 15:12:02 +0100

    Add table button

--- assets/formElement.js
+++ assets/formElement.js
@@ -6,7 +6,9 @@ addButtons.forEach(button => {
 		const collectionHolder = document.querySelector(button.dataset.collection);
 
 		const item = document.createElement('div');
-		item.classList.add('mb-2');
+		if (button.dataset.collection !== '#tables') {
+			item.classList.add('mb-2');
+		}
 		item.innerHTML = collectionHolder
 				.dataset
 				.prototype
@@ -15,7 +17,29 @@ addButtons.forEach(button => {
 						collectionHolder.dataset.index
 				);
 		item.querySelector('.btn-remove').addEventListener('click', () => item.remove());
-		collectionHolder.appendChild(item);
+
+		// ajout des events +- sur l'input number pour la gestion des tables
+		if (button.dataset.collection === '#tables') {
+			const plusBtn = item.querySelector('.number-plus');
+			plusBtn.addEventListener('click', () => {
+				const input = plusBtn.parentElement.querySelector('input[type=number]');
+				input.valueAsNumber += 1;
+			})
+			const minBtn = item.querySelector('.number-minus');
+			minBtn.addEventListener('click', () => {
+				const input = minBtn.parentElement.querySelector('input[type=number]');
+				if (input.valueAsNumber > input.min) {
+					input.valueAsNumber -= 1;
+				}
+			})
+			item.querySelector('input[type=number]').value = 1;
+		}
+
+		if (button.dataset.collection === '#tables') {
+			document.querySelector('#table-holder').appendChild(item);
+		} else {
+			collectionHolder.appendChild(item);
+		}
 		collectionHolder.dataset.index++;
 	});
 })
