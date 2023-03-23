90e4828c96e8c223a37ca9884c6d5aabd8a4adfa
VERBRUGGEN Frédéric <tekodragoon@gmail.com>
2023-03-23 23:38:01 +0100

    schedule page

--- src/Twig/DayOfWeek.php
+++ src/Twig/DayOfWeek.php
@@ -10,11 +10,12 @@ class DayOfWeek extends AbstractExtension
     public function getFilters():array
     {
         return [
-            new TwigFilter('dayOfWeek', [$this, 'dayOfWeek'])
+            new TwigFilter('dayOfWeek', [$this, 'dayOfWeek']),
+            new TwigFilter('frDayOfWeek', [$this, 'frDayOfWeek']),
         ];
     }
 
-    public function dayOfWeek(string $day):string
+    public function dayOfWeek(int $day):string
     {
         $days = [
             0 => 'Monday',
@@ -24,6 +25,20 @@ class DayOfWeek extends AbstractExtension
             4 => 'Friday',
             5 => 'Saturday',
             6 => 'Sunday'
+        ];
+        return $days[$day];
+    }
+
+    public function frDayOfWeek(int $day):string
+    {
+        $days = [
+            0 => 'Lundi',
+            1 => 'Mardi',
+            2 => 'Mercredi',
+            3 => 'Jeudi',
+            4 => 'Vendredi',
+            5 => 'Samedi',
+            6 => 'Dimanche'
         ];
         return $days[$day];
     }
