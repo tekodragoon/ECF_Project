70be49fcee7374c40578bf31c20e66560bc9a4e9
VERBRUGGEN Frédéric <tekodragoon@gmail.com>
2023-04-06 18:51:32 +0200

    update mail

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,13 +4,13 @@ namespace App\Controller;
 
 use App\Form\ContactType;
 use App\Model\Message;
+use Psr\Log\LoggerInterface;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
@@ -18,22 +18,26 @@ use Symfony\Contracts\Translation\TranslatorInterface;
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(Request $request, MailerInterface $mailer, TranslatorInterface $translator): Response
+    public function index(Request $request,
+                          MailerInterface $mailer,
+                          TranslatorInterface $translator,
+                          LoggerInterface $logger,
+    ): Response
     {
         $message = new Message();
         $form = $this->createForm(ContactType::class, $message);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            //TODO: Change to TemplatedEmail
             $email = (new TemplatedEmail())
                 ->to($message->getEmail())
-                ->subject('Confirmation de reception')
+                ->subject($translator->trans('message.confirmReceived'))
                 ->htmlTemplate('contact/confirmEmailSent.html.twig')
                 ->context([
                     'message' => $message->getContent(),
                     'name' => $message->getName(),
                     'subject' => $message->getSubject(),
+                    'locale' => $request->getLocale(),
                 ]);
             $messageEmail = (new TemplatedEmail())
                 ->to($this->getParameter('app.contact.email'))
@@ -44,13 +48,14 @@ class ContactController extends AbstractController
                     'name' => $message->getName(),
                     'subject' => $message->getSubject(),
                     'emailSender' => $message->getEmail(),
+                    'locale' => $request->getLocale(),
                 ]);
             try {
                 $mailer->send($messageEmail);
                 $mailer->send($email);
                 $this->addFlash('success', $translator->trans('message.sended'));
             } catch (TransportExceptionInterface $e) {
-                //TODO: log error
+                $logger->error($e->getMessage());
                 $this->addFlash('error', $translator->trans('message.problem'));
             }
             $this->redirectToRoute('app_contact');
