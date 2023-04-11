1484bdd13aa04772f46a3951207cffda0e98fdba
VERBRUGGEN Frédéric <tekodragoon@gmail.com>
2023-04-11 12:16:41 +0200

    add locale

--- src/Controller/BookingController.php
+++ src/Controller/BookingController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/{_locale}')]
 class BookingController extends AbstractController
 {
     #[Route('/booking', name: 'app_booking')]
