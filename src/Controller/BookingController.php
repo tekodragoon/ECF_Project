30828689a72edb1646a6e8d3f258a9b967aebf6f
VERBRUGGEN Frédéric <tekodragoon@gmail.com>
2023-04-17 21:35:55 +0200

    update controller

--- src/Controller/BookingController.php
+++ src/Controller/BookingController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\ReservationRepository;
 use DateTime;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,30 +13,39 @@ use Symfony\Component\Routing\Annotation\Route;
 class BookingController extends AbstractController
 {
     #[Route('/booking', name: 'app_booking')]
-    public function index(): Response
+    public function index(ReservationRepository $repository): Response
     {
-        $year = date('Y',strtotime('last monday'));
-        $month = date('m',strtotime('last monday'));
-        $day = date('d',strtotime('last monday'));
+        $year = date('Y',strtotime('monday this week'));
+        $month = date('m',strtotime('monday this week'));
+        $day = date('d',strtotime('monday this week'));
+
+        $bookings = $repository->findWeek(new DateTime('monday this week'));
 
         return $this->render('booking/index.html.twig', [
             'year' => $year,
             'month' => $month,
             'day' => $day,
+            'bookings' => $bookings,
         ]);
     }
 
+    /**
+     * @throws Exception
+     */
     #[Route('/booking/{date}', name: 'app_booking_date')]
-    public function indexDate(string $date): Response
+    public function indexDate(string $date, ReservationRepository $repository): Response
     {
         $year = date('Y',strtotime($date));
         $month = date('m',strtotime($date));
         $day = date('d',strtotime($date));
 
+        $bookings = $repository->findWeek(new DateTime($date));
+
         return $this->render('booking/show-date.html.twig', [
             'year' => $year,
             'month' => $month,
             'day' => $day,
+            'bookings' => $bookings,
         ]);
     }
 }
